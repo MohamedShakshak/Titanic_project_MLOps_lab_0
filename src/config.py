b698dee71ab5c0c3f10c37bad8e71a89b4e26f36
Mohamed Shakshak <mohamedshakshak455@gmail.com>
2026-04-21 17:15:18 +0200

    edit config file

--- src/config.py
+++ src/config.py
@@ -1,32 +1,11 @@
 from pathlib import Path
 
-from dotenv import load_dotenv
-from loguru import logger
+# Project root
+ROOT_DIR = Path(__file__).resolve().parents[1]
 
-# Load environment variables from .env file if it exists
-load_dotenv()
-
-# Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
-
-DATA_DIR = PROJ_ROOT / "data"
+# Data paths
+DATA_DIR = ROOT_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
-MODELS_DIR = PROJ_ROOT / "models"
-
-REPORTS_DIR = PROJ_ROOT / "reports"
-FIGURES_DIR = REPORTS_DIR / "figures"
-
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
-try:
-    from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
+# Kaggle dataset
+KAGGLE_DATASET = "competitions/titanic"
