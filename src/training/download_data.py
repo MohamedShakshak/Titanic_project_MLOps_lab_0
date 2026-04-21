0d213e7e8d939b7a7ad3cd7a344cca84031458e9
Mohamed Shakshak <mohamedshakshak455@gmail.com>
2026-04-21 17:34:39 +0200

    edit download_data script

--- src/training/download_data.py
+++ src/training/download_data.py
@@ -6,24 +6,11 @@ from src.config import RAW_DATA_DIR
 
 
 def download_data():
-    """
-    Download Titanic dataset using kagglehub
-    """
-
     RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
+    path = kagglehub.dataset_download("yasserh/titanic-dataset")
 
-    # Example dataset (public Titanic dataset)
-    path = kagglehub.dataset_download("heptapod/titanic")
-
-    print(f"Downloaded to: {path}")
-
-    # Copy files into data/raw
     for file in Path(path).glob("*"):
-        shutil.copy(file, RAW_DATA_DIR / file.name)
+        shutil.copy(file, RAW_DATA_DIR / "train.csv")
 
-    print(f"Data copied to {RAW_DATA_DIR}")
-
-
-if __name__ == "__main__":
-    download_data()
+    print("Data downloaded to raw/")
